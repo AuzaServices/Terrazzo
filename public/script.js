c89b3b3ebabf78e3f48a6a2db5488b7cbe05c1c6
Davi <andrielysom@gmail.com>
2025-07-27 18:44:16 -0300

    reservas

--- public/script.js
+++ public/script.js
@@ -169,7 +169,7 @@ function abrirFormulario(idDia, dia, mes, ano) {
       })
     }).then(() => {
       document.body.removeChild(overlay);
-      carregarAgendamentosDoBanco();
+      carregarAgendamentosDoBanco(); // só atualiza o calendário após agendar
     }).catch(err => {
       alert("Erro ao agendar. Tente novamente.");
       console.error(err);
@@ -192,5 +192,4 @@ btnProximo.onclick = () => {
   criarCalendario(mesAtual, anoAtual);
 };
 
-carregarAgendamentosDoBanco();
-setInterval(carregarAgendamentosDoBanco, 1000);
+carregarAgendamentosDoBanco(); // só executa uma vez ao abrir a página
